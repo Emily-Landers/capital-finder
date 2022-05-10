9f53c6c3fbf2350f1c4375648bcb92760205d17b
Emily Landers <emily.j.landers@gmail.com>
2022-05-09 21:29:20 -0400

    basic outline

--- api/capital.py
+++ api/capital.py
@@ -1,11 +1,26 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import requests
+#import requests
 
 class handler(BaseHTTPRequestHandler):
+    
     def do_GET(self):
+        BASE_URL = "https://restcountries.com/v3.1"
+        path = self.path
+        url_components = parse.urlsplit(path)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header("content-type", "text/plain")
         self.end_headers()
-        self.wfile.write("Hello World".encode())
+        
+        country = dic.get("country")
+        capital = dic.get("capital")
+        
+        message = f"The capital of {country} is {capital}"
+        
+        self.wfile.write(message.encode())
         return
+
+#E.g./capital-finder?capital=Santiago should generate an http response of Santiago is the capital of Chile.
